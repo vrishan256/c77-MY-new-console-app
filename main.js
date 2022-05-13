5d04613c0ea385c0e4b551bcd7aaf84f829ec37d
vrishan256 <88889787+vrishan256@users.noreply.github.com>
2022-05-13 21:32:01 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+var firstname= "Ranbir";
+var secondname="singh";
+var fullname= firstname.concat(secondname);
+console.log(fullname);
+ console.log("My first console web app");      
+ 
+ var a= 6;
+ var b= 11;
+ console.log(a+b);
+ console.log(a-b);
+ console.log(a*b);
+ console.log(a/b);
